1dabbf31672a0ebc5116a79dee57fe7d6cf83d6a
Rodrigo Sanders <contato@eloom.com.br>
2019-10-12 19:58:35 -0300

    Validadores

---
+++ src/module-dholi-jadlog/view/frontend/web/js/model/shipping-rates-validation-rules.js
@@ -0,0 +1,1 @@
+define([],function(){return{getRules:function(){return{postcode:{required:true},country_id:{required:true},region_id:{required:true},city:{required:true}}}}});
---
+++ src/module-dholi-jadlog/view/frontend/web/js/view/shipping-rates-validation.js
@@ -0,0 +1,1 @@
+define(["uiComponent","Magento_Checkout/js/model/shipping-rates-validator","Magento_Checkout/js/model/shipping-rates-validation-rules","Dholi_Jadlog/js/model/shipping-rates-validator","Dholi_Jadlog/js/model/shipping-rates-validation-rules"],function(b,c,a,d,e){c.registerValidator("dholi_jadlog",d);a.registerRules("dholi_jadlog",e);return b});
